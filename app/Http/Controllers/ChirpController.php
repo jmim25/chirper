758ad6e840cf6e92d184289269504137b043626d
Juan Miguel I. Moreno <juan.miguel.im25@gmail.com>
2024-05-22 11:51:25 -0600

    update / delete

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,6 +6,7 @@ use App\Models\Chirp;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class ChirpController extends Controller
@@ -33,9 +34,6 @@ class ChirpController extends Controller
         //
     }
 
-
-    // TODO bootcamp   Saving the Chirp https://bootcamp.laravel.com/blade/creating-chirps
-
     /**
      * Store a newly created resource in storage.
      */
@@ -61,24 +59,34 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Chirp $chirp)
+    public function edit(Chirp $chirp): View
     {
-        //
+        Gate::authorize('update', $chirp);
+		return view('chirps.edit', [
+			'chirp' => $chirp,
+		]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        Gate::authorize('update', $chirp);
+		$validated = $request->validate([
+			'message' => 'required|string|max:255',
+		]);
+		$chirp->update($validated);
+		return redirect(route('chirps.index'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Chirp $chirp)
+    public function destroy(Chirp $chirp): RedirectResponse
     {
-        //
+        Gate::authorize('update', $chirp);
+		$chirp->delete();
+		return redirect(route('chirps.index'));
     }
 }
